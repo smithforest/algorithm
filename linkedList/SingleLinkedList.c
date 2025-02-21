025757ab474b02c4c54deae8fe505249ede292bf
smithforest@126.com <smithforest@126.com>
2025-02-21 11:02:28 +0800

    提交代码

--- linkedList/SingleLinkedList.c
+++ linkedList/SingleLinkedList.c
@@ -1,1 +1,43 @@
 #include "SingleLinkedList.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * 初始化单链表
+ */
+SingleLinkedList init_list()
+{
+    SingleLinkedList l = (SingleLinkedList)malloc(sizeof(ListNode));
+    l->next = NULL;
+    return l;
+}
+
+
+/**
+ * 尾插法创建单链表
+ */
+SingleLinkedList create_list_rear()
+{
+    SingleLinkedList l = init_list();
+    ListNode* rear = l;
+    int x;
+    while (scanf("%d", &x) != EOF)
+    {
+        ListNode* p = (ListNode*)malloc(sizeof(ListNode));
+        p->data = x;
+        rear->next = p;
+        rear = p;
+    }
+    return l;
+}
+
+/**
+ * 顺序输出单链表
+ */
+void print_list(SingleLinkedList L)
+{
+
+
+}
